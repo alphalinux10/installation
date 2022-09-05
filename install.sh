28360975093604e6429130be6ff7487e026bfd5c
alpha <alpha.linux@protonmail.com>
2022-09-05 23:51:19 +0200

    update

--- install.sh
+++ install.sh
@@ -75,6 +75,27 @@ ping archlinux.org
 "
 }
 
+function installSudoPacman(){
+	for n in $(seq 1 $#); do
+		sudo pacman --noconfirm --needed -S ${!n}
+		if [[ "$?" == "1" ]];then echo "${!n}" >> install-error.tmp; fi
+	done
+}
+
+function installPacman(){
+	for n in $(seq 1 $#); do
+		pacman --noconfirm --needed -S ${!n}
+		if [[ "$?" == "1" ]];then echo "${!n}" >> install-error.tmp; fi
+	done
+}
+
+function installParu(){
+	for n in $(seq 1 $#); do
+		paru --noconfirm --needed -S ${!n}
+		if [[ "$?" == "1" ]];then echo "${!n}" >> installParu-error.tmp; fi
+	done
+}
+
 function install() {
 	# mirrorlist
 	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
@@ -93,7 +114,7 @@ function install() {
 function archChroot() {
 	#### 1) Time zone
 		ln -sf /usr/share/zoneinfo/Europe/Bratislava /etc/localtime
-                hwclock --systohc
+        hwclock --systohc
 
         #### 2) Locale
 		echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
@@ -102,44 +123,64 @@ function archChroot() {
 		echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
         #### 3) Hostname & network configuration
-		HOSTNAME="ALPHA - $(dmidecode -s system-version)"
-                echo $HOSTNAME > /etc/hostname
-                echo "127.0.0.1         localhost" > /etc/hosts
-                echo "::1               localhost" >> /etc/hosts
-                echo "127.0.0.1         $HOSTNAME.localdomain           $HOSTNAME" >> /etc/hosts
+		installPacman dmidecode 
+		HOSTNAME="ALPHA-$(dmidecode -s system-version|sed"s/[[:space:]]/_/g")"
+        echo $HOSTNAME > /etc/hostname
+        echo "127.0.0.1         localhost" > /etc/hosts
+        echo "::1               localhost" >> /etc/hosts
+        echo "127.0.0.1         $HOSTNAME.localdomain           $HOSTNAME" >> /etc/hosts
 
         #### 5) Linux core packages & drivers & another needed packages
-                ### Base
-		pacman --noconfirm --needed -S base-devel git
-                ### Drivers
-                pacman --noconfirm --needed -S xf86-video-intel libgl mesa intel-ucode intel-gmmlib intel-media-driver intel-media-sdk intel-tbb nvidia nvidia-lts nvidia-libgl xf86-video-amdgpu xf86-video-nouveau xf86-video-ati xf86-video-vesa virtualbox-guest-utils virtualbox-guest-modules-arch mesa-libgl
+        ### Base
+		installPacman base-devel git
+        ### Drivers
+		if [[ $(lspci | awk '/VGA/ {print $5}') == "Advanced" ]]; then
+        	# amd
+        	installPacman libgl mesa xf86-video-amdgpu mesa-libgl
+		elif [[ $(lspci | awk '/VGA/ {print $5}') == "Intel" ]]; then
+			# intel
+        	installPacman xf86-video-intel libgl mesa intel-gmmlib intel-media-driver intel-media-sdk intel-tbb mesa-libgl 
+		else
+			# other
+        	installPacman xf86-video-intel libgl mesa intel-ucode intel-gmmlib intel-media-driver intel-media-sdk intel-tbb nvidia nvidia-lts nvidia-libgl xf86-video-amdgpu xf86-video-nouveau xf86-video-ati xf86-video-vesa virtualbox-guest-utils virtualbox-guest-modules-arch mesa-libgl
+		fi
 		### Network
-                pacman --noconfirm --needed -S networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools
-                ### Bluetooth & Printing
-                pacman --noconfirm --needed -S bluez bluez-utils pulseaudio-bluetooth cups xdg-utils xdg-user-dirs acpi hplip 
+        installPacman networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools iwd
+        systemctl enable NetworkManager.service
+        ### Bluetooth
+        installPacman bluez bluez-utils 
+        systemctl enable bluetooth.service
 		### Sound
-		pacman --noconfirm --needed -S alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack wireplumber 
-		### Other
-		pacman --noconfirm --needed -S bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid ntfs-3g terminus-font avahi gvfs gvfs-smb nfs-utils inetutils dnsutils
-
-        #### 8) Enable network manager & bluetooth & printing
-                systemctl enable NetworkManager
-                systemctl enable bluetooth
+		installPacman alsa-utils pipewire pipewire-docs pipewire-alsa pipewire-pulse pipewire-jack wireplumber qpwgraph
+        systemctl enable pipewire-pulse.service
+        ### Printing
+        installPacman cups xdg-utils xdg-user-dirs ghostscript gsfonts gutenprint gtk3-print-backends libcups hplip system-config-printer 
 		systemctl enable cups.service
+		### Scanning
+		installPacman sane ipp-usb sane-airscan simple-scan
+		systemctl enable ipp-usb.service
+		### Virt-manager
+        installPacman virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft
+		systemctl enable libvirtd.service
+		### System information
+		installPacman usbutils hwinfo neofetch sysstats
 
-	#### 9) Root password
+		### Other
+		installPacman bash-completion openssh rsync reflector acpi acpi_call acpid tlp bridge-utils vde2 openbsd-netcat ipset firewalld flatpak sof-firmware nss-mdns acpid ntfs-3g terminus-font avahi gvfs gvfs-smb nfs-utils inetutils dnsutils
+
+	#### 6) Root password
 		echo root:password | chpasswd
 
-        #### 9) Create user
+        #### 7) Create user
 		useradd -m alpha
 		echo alpha:password | chpasswd
 		usermod -aG libvirt alpha
 
-        #### 10) Sudo permitions of user
-                pacman  --noconfirm --needed -S sudo
+        #### 8) Sudo permitions of user
+        pacman  --noconfirm --needed -S sudo
 		echo "alpha ALL=(ALL) ALL" >> /etc/sudoers.d/alpha
 
-        #### 7) Initramfs
+        #### 9) Initramfs
 		if [[ $(lspci | awk '/VGA/ {print $5}') == "Advanced" ]]; then
                 	sed -i '/^MODULES/s/.*/MODULES=(btrfs amdgpu)/i' /etc/mkinitcpio.conf
 		else
@@ -147,20 +188,21 @@ function archChroot() {
 		fi
 		mkinitcpio -P
         
-	##### 6) Boot loader
-                pacman --noconfirm --needed -S grub efibootmgr
+	##### 10) Boot loader
+        installPacman grub efibootmgr
 		grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 		grub-mkconfig -o /boot/grub/grub.cfg
 
         #### 11) Journal
         ###        sed -i '/Storage=auto/s/.*/Storage=volatile;/RuntimeMaxUse=/s/.*/RuntimeMaxUse=30M' /etc/systemd/journald.conf
 }
+
 function postInstall() {
 	# Update mirrorlist
 	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 	reflector -c Slovakia -c Czechia -a 10 --sort rate --save /etc/pacman.d/mirrorlist
 	### Synchronize pacman pkgs
-	sudo pacman -Syy
+	sudo pacman --noconfirm --needed -Syy
 	### Syncronize time protocol
 	sudo timedatectl set-ntp true
 	sudo hwclock --systohc
@@ -177,67 +219,105 @@ function postInstall() {
 	cd paru-bin
 	makepkg -si
 	cd
+
 	### TIMESHIFT
-	paru -S timeshift timeshift-autosnap
+	installParu timeshift timeshift-autosnap
 	timeshift
 
 	### ZRAMD
-	paru -S zramd
+	installParu zramd
 	sudo systemctl enable --now zramd.service
 	lsblk
 
 	### SDDM
-	sudo pacman --noconfirm --needed -S sddm
-	sudo systemctl enable --now sddm
+	installSudoPacman sddm
+	sudo systemctl enable --now sddm.service
 
-	### OTHER
-	sudo pacman --noconfirm --needed -S awesome awesome-terminal-fonts alacritty brave-bin firefox dmenu
-	sudo pacman --noconfirm --needed -S xorg-server xorg-apps xorg-xinit xf86-input-libinput libinput xf86-input-evdev xf86-input-wacom
-	sudo pacman --noconfirm --needed -S xdo bash-completion git wget zip gzip unzip
-	sudo pacman --noconfirm --needed -S dunst flameshot udevil thunar sxiv libreoffice
-	sudo pacman --noconfirm --needed -S arandr simplescreenrecorder arc-gtk-theme arc-icon-theme dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts archlinux-wallpaper playerctl scrot obs-studio dunst pacman-contrib
+	##Touchscreen
+	if [[ $(lsusb | grep "Touch") != '' ]]; then
+		echo "MOZ_USE_XINPUT2 DEFAULT=1" | sudo tee -a /etc/security/pam_env.conf
+	fi
+	#Touchpad
+	installSudoPacman xf86-input-synaptics
+	cp $HOME/installation/conf/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
 
-		# printers
-	sudo pacman --noconfirm --needed -S ghostscript gsfonts gutenprint gtk3-print-backends libcups hplip system-config-printer
-		# fonts
-	sudo pacman --noconfirm --needed -S adobe-source-sans-pro-fonts cantarell-fonts noto-fonts terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font
+	mkdir -p ~/Programs
+	# Programs
+		## XORG
+		installSudoPacman xorg-server xorg-apps xorg-xinit xf86-input-libinput libinput xf86-input-evdev xf86-input-wacom
+		## Web Browsers
+		installSudoPacman brave-bin firefox librewolf
+		## Terminals
+		installSudoPacman alacritty rxvt-unicode
+		installParu st
+		## WM
+		installSudoPacman awesome dmenu
+		# Editors
+		installSudoPacman code
+        		### neovim
+        		cd Programs
+			installSudoPacman cmake unzip ninja tree-sitter curl
+        		git clone https://github.com/neovim/neovim.git
+        		cd neovim
+        		git checkout release-0.7
+        		make CMAKE_BUILD_TYPE=Release
+        		sudo make install
+        		#git clone https://github.com/LunarVim/nvim-basic-ide.git ~/.config/nvim
+        		cd ..
+			installSudoPacman xsel wl-clipboard pip npm ripgrep
+        		#pip install pynvim
+        		#npm i -g neovim
+        		### xdg-ninja
+        		git clone https://github.com/b3nj5m1n/xdg-ninja.git
+        		sudo pacman -S jq glow
+		# Tools
+		installSudoPacman xdo bash-completion git wget dunst udevil arandr youtube-dl htop iio-sensor-proxy inotify-tools redshift conky 
+		## File Managers
+			# xplr
+			installSudoPacman xplr imv xdotool
+			# thunar
+			installSudoPacman thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman meld
+			# nemo
+    		installSudoPacman $(sudo pacman -Ss nemo | grep "community/" | awk '{print $1}' | sed "s<community/<<g" | tr '\n' ' ')
+			# other
+			installSudoPacman lf clifm
 
+		## Docs
+		installSudoPacman libreoffice onlyoffice-bin
+		## ImageEditors
+		installSudoPacman sxiv feh gimp inkscape blender
+		## Image/Video capture
+		installSudoPacman flameshot scrot simplescreenrecorder obs-studio
+		## Video players
+		installSudoPacman vlc mpv
+		## Compression
+		installSudoPacman zip gzip unzip
+		## Fonts
+		installSudoPacman awesome-terminal-fonts adobe-source-sans-pro-fonts bdf-unifont cantarell-fonts dina-font noto-fonts terminus-font ttf-bitstream-vera ttf-dejavu ttf-droid ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family tamsyn-font ttf-croscore gnu-free-fonts ttf-ibm-plex ttf-linux-libertine tex-gyre-fonts ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-jetbrains-mono ttf-monofur inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome 
+		## Themes
+		installSudoPacman arc-gtk-theme arc-icon-theme 
+		## Other
+		installSudoPacman archlinux-wallpaper playerctl pacman-contrib
 
-	sudo pacman -noconfirm --needed -S lf youtube-dl feh htop iio-sensor-proxy inotify-tools redshift conky xplr clifm thunar
-
+	#installSudoPacman xorg-server xorg-apps xorg-xinit xf86-input-libinput libinput xf86-input-evdev xf86-input-wacom
+	#installSudoPacman arandr simplescreenrecorder arc-gtk-theme arc-icon-theme dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts archlinux-wallpaper playerctl scrot obs-studio dunst pacman-contrib
     #sudo pacman -noconfirm --needed -S mypaint
 
-#### c)
-
-	sudo pacman -S android-tools android-udev
-	paru -S scrcpy sndcpy-bin
-
-	sudo pacman -S sysstats
-
-		#Touchscreen
-		echo "MOZ_USE_XINPUT2 DEFAULT=1" | sudo tee -a /etc/security/pam_env.conf
-		
-		#Touchpad
-		sudo pacman -S xf86-input-synaptics
-		cp $HOME/installation/conf/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
+	installSudoPacman android-tools android-udev scrcpy
+	installParu sndcpy-bin 
 		
 		#Network
-		sudo pacman -S network-manager-applet
 		paru -S networkmanager-dmenu-git
 
 		#card reader
-		#sudo pacman -S ccid libnfc acsccid pcsclite pcsc-tools
-		#sudo systemctl enable pcscd
-		#sudo systemctl start pcscd
+		sudo pacman -S ccid libnfc acsccid pcsclite pcsc-tools
+		sudo systemctl enable pcscd
 
 	sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 	sudo firewall-cmd --add-port=1025-65535/udp --permanent
 	sudo firewall-cmd --reload
 
 	#sudo pacman -S --noconfirm picom nitrogen lxappearance dmenu arandr simplescreenrecorder alsa-utils pulseaudio alsa-utils pulseaudio-alsa pavucontrol arc-gtk-theme arc-icon-theme dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome awesome-terminal-fonts archlinux-wallpaper playerctl scrot obs-studio dunst pacman-contrib
-
-
-	sudo pacman -S --noconfirm alacritty xorg sddm firefox simplescreenrecorder obs-studio vlc mpv 
 
 		systemctl enable sshd
 		systemctl enable avahi-daemon
